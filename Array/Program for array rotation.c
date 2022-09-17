2a07afef85c82320a32171da0b70693341e3cfba
ankitmishra1201 <workwithankit12@gmail.com>
2022-09-17 13:22:15 +0530

    Initial Commit

---
+++ Array/Program for array rotation.c
@@ -0,0 +1,17 @@
+// Given an array of integers arr[] of size N and an integer, the task is to rotate the array elements to the left by d positions.
+
+// Approach 1 (Using temp array): This problem can be solved using the below idea:
+
+// After rotating d positions to the left, the first d elements become the last d elements of the array
+
+// First store the elements from index d to N-1 into the temp array.
+// Then store the first d elements of the original array into the temp array.
+// Copy back the elements of the temp array into the original array
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    void ar
+    return 0;
+}
